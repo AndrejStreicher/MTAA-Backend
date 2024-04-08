b6bd820630d0109ec381f43bf15f38b9aaabeb36
Andrej Streicher <andrejstreicher1@gmail.com>
2024-04-08 13:18:09 +0200

    Initial project setup

---
+++ src/main/kotlin/fiit/mtaa/yourslovakia/YourSlovakiaApplication.kt
@@ -0,0 +1,11 @@
+package fiit.mtaa.yourslovakia
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YourSlovakiaApplication
+
+fun main(args: Array<String>) {
+	runApplication<YourSlovakiaApplication>(*args)
+}
---
+++ src/test/kotlin/fiit/mtaa/yourslovakia/YourSlovakiaApplicationTests.kt
@@ -0,0 +1,13 @@
+package fiit.mtaa.yourslovakia
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YourSlovakiaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
