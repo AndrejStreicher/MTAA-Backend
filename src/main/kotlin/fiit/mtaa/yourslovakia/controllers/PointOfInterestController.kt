357956e84a54e7b611962fb522fb15a8fae05a37
Andrej Streicher <andrejstreicher1@gmail.com>
2024-04-15 21:03:32 +0200

    Controller for PointsOfInterest

--- src/main/kotlin/fiit/mtaa/yourslovakia/controllers/PointOfInterestController.kt
+++ src/main/kotlin/fiit/mtaa/yourslovakia/controllers/PointOfInterestController.kt
@@ -1,13 +1,29 @@
 package fiit.mtaa.yourslovakia.controllers
 
 import fiit.mtaa.yourslovakia.models.GeoPoint
-import org.springframework.stereotype.Controller
+import fiit.mtaa.yourslovakia.models.PointOfInterest
+import fiit.mtaa.yourslovakia.services.PointOfInterestService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
-class PointOfInterestController {
+@RestController
+class PointOfInterestController(private val pointOfInterestService: PointOfInterestService) {
+
     @GetMapping("/pointsofinterest")
-    fun getPointsOfInterestByLocation(location: GeoPoint) {
-
+    fun getPointsOfInterestByLocation(
+        @RequestParam latitude: Float,
+        @RequestParam longitude: Float,
+        @RequestParam maxDistance: Double,
+        @RequestParam monumentTypes: List<String>
+    ): ResponseEntity<List<PointOfInterest>> {
+        val pointsOfInterests =
+            pointOfInterestService.getMonuments(GeoPoint(latitude, longitude), maxDistance, monumentTypes)
+        return if (pointsOfInterests.isEmpty()) {
+            ResponseEntity.noContent().build()
+        } else {
+            ResponseEntity.ok(pointsOfInterests)
+        }
     }
 }
