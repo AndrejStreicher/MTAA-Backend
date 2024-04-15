5b79e1e46204548a66caee5139f527378fd9e36c
Tomas Kubrican <tomkubri@gmail.com>
2024-04-15 17:33:07 +0200

    Changed the way location is used

--- src/main/kotlin/fiit/mtaa/yourslovakia/controllers/PointOfInterestController.kt
+++ src/main/kotlin/fiit/mtaa/yourslovakia/controllers/PointOfInterestController.kt
@@ -1,12 +1,13 @@
 package fiit.mtaa.yourslovakia.controllers
 
+import fiit.mtaa.yourslovakia.models.GeoPoint
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class PointOfInterestController {
     @GetMapping("/pointsofinterest")
-    fun getPointsOfInterestByLocation(latitude: Float, longitude: Float) {
+    fun getPointsOfInterestByLocation(location: GeoPoint) {
 
     }
 }
